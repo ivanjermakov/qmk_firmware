85ab74a3d89d086c27079d7529001a0a5c531a75
ivanjermakov <ivanjermakov1@gmail.com>
2021-02-21 23:03:26 +0300

    Layout, lighting

--- keyboards/crkbd/keymaps/home36/config.h
+++ keyboards/crkbd/keymaps/home36/config.h
@@ -17,12 +17,13 @@
 
 #ifdef RGBLIGHT_ENABLE
     #undef RGBLED_NUM
-    #define RGBLIGHT_ANIMATIONS
     #define RGBLED_NUM 27
     #define RGBLIGHT_LIMIT_VAL 120
     #define RGBLIGHT_HUE_STEP 10
-    #define RGBLIGHT_SAT_STEP 17
-    #define RGBLIGHT_VAL_STEP 17
+    #define RGBLIGHT_SAT_STEP 25
+    #define RGBLIGHT_VAL_STEP 25
+
+    #define RGBLIGHT_EFFECT_RAINBOW_MOOD
 #endif
 
 #define OLED_FONT_H "keyboards/crkbd/lib/glcdfont.c"
